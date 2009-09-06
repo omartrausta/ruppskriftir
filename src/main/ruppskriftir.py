f391433d0f93a60771e9a5bcce5935ebf8bb20e7
davidhalldor <davidhalldor@bdcd9674-9aee-11de-bff5-93fd3790126f>
2009-09-06 15:44:35 +0000

    Bjó til nokkur föll og test föll

--- src/main/ruppskriftir.py
+++ src/main/ruppskriftir.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Ruppskriftir:
     def __init__(self):
         self.magn = ""
@@ -5,10 +7,19 @@ class Ruppskriftir:
         self.vara = ""
         self.nafnInnihald = ""
         self.endurkvaemt = False
-        
-    def innihald(self,magn,eining,vara,innihald="",endurkvaemt=False):
+    
+    # Falls sem á að skrifa út umbeðna uppskrift    
+    def innihald(self, magn, eining, vara, innihald="", endurkvaemt=False):
         pass
     
-    # Fall gæti tekið 
+    # Fall gæti tekið klasa af uppskriftum
     def buaTilUppskrift(self):
+        pass
+    
+    # Fall sem skrifar í skrá
+    def skrifaSkra(self):
+        pass
+
+    # Fall sem les uppúr skrá
+    def lesaSkra(self):
         pass
