379b7166a769cecb47b665d4bbf8afa46539f62a
davidhalldor <davidhalldor@bdcd9674-9aee-11de-bff5-93fd3790126f>
2009-09-09 10:21:52 +0000

    Lagaði kóðan með auto-formatteringu

--- trunk/src/main/ruppskriftir.py
+++ trunk/src/main/ruppskriftir.py
@@ -1,10 +1,10 @@
 # encoding: utf-8
-import os, sys, pickle 
+import os, sys
 from fractions import Fraction
 
 class Ruppskriftir:
     
-    def __init__(self,currdir=None):
+    def __init__(self, currdir=None):
         self.magn = ""
         self.eining = ""
         self.vara = ""
@@ -30,7 +30,7 @@ class Ruppskriftir:
             #----------------------            
             file = open(path, 'r')
             line = file.readline()
-            while len(line)> 0:
+            while len(line) > 0:
                 line = line.rstrip()
                 dalkar = line.split()
                 if len(dalkar) == 4:
@@ -49,37 +49,37 @@ class Ruppskriftir:
                     #----------------------
                     if dalkarForm[2] == dalkarForm[6]:
                         #print dalkarForm[2]
-                        fractEin = Fraction(int(dalkarForm[4]),int(dalkarForm[0]))
+                        fractEin = Fraction(int(dalkarForm[4]), int(dalkarForm[0]))
                         eining = dalkarForm[5]
                         unit = dalkarForm[1]
-                        tup = [(unit,fractEin,eining)]
+                        tup = [(unit, fractEin, eining)]
                         if einingar.has_key(dalkarForm[2]):
                             tupEin = einingar[dalkarForm[2]] + tup
-                            einingar[dalkarForm[2]]=tupEin
+                            einingar[dalkarForm[2]] = tupEin
                         else:
-                            einingar[dalkarForm[2]]=tup
+                            einingar[dalkarForm[2]] = tup
                     #----------------------
                     # setja inn verð á einingum í verd
                     #----------------------                            
                     if dalkarForm[6] == "peningar":
-                        verdPerUnit = float(dalkarForm[4])/float(dalkarForm[0])
-                        verd[dalkarForm[2]]=(dalkarForm[1],verdPerUnit)
+                        verdPerUnit = float(dalkarForm[4]) / float(dalkarForm[0])
+                        verd[dalkarForm[2]] = (dalkarForm[1], verdPerUnit)
                     #----------------------
                     # setja inn í uppskriftir og uppskriftirUnit
                     # uppskrfitirUnit halda utan um grunneiningu sem fæst með því að 
                     # vinna úr uppskrift
                     #----------------------
                     if dalkarForm[2] != dalkarForm[6] and dalkarForm[6] != "peningar":
-                        magn = Fraction(int(dalkarForm[4]),int(dalkarForm[0]))
+                        magn = Fraction(int(dalkarForm[4]), int(dalkarForm[0]))
                         eining = dalkarForm[5]
                         innih = dalkarForm[6]
-                        tup = [(magn,eining,innih)]
+                        tup = [(magn, eining, innih)]
                         if uppskrift.has_key(dalkarForm[2]):
                             tupEin = uppskrift[dalkarForm[2]] + tup
-                            uppskrift[dalkarForm[2]]=tupEin
+                            uppskrift[dalkarForm[2]] = tupEin
                         else:
-                            uppskriftUnit[dalkarForm[2]]=(1,dalkarForm[1])
-                            uppskrift[dalkarForm[2]]=tup                            
+                            uppskriftUnit[dalkarForm[2]] = (1, dalkarForm[1])
+                            uppskrift[dalkarForm[2]] = tup                            
                 line = file.readline()  
                 
             print einingar
@@ -94,7 +94,7 @@ class Ruppskriftir:
             print "Unexpected error:", sys.exc_info()[0]
             raise
       
-    def setPath(self,currdir):
+    def setPath(self, currdir):
         if currdir == None:
             os.chdir(os.getcwd() + '/../..')
         else:
@@ -107,6 +107,6 @@ class Ruppskriftir:
 def main():
     uppsrkiftir = Ruppskriftir()
     listi = uppsrkiftir.lesaSkra()
-    #print listi
+    print listi
     
-if __name__== "__main__": main()
+if __name__ == "__main__": main()
