bddc6b4f4cb0ab3c6d5f1c605b9b2fa3b9cfb19c
omar.traustason@gmail.com <omar.traustason@gmail.com@bdcd9674-9aee-11de-bff5-93fd3790126f>
2009-09-09 03:44:08 +0000

    Les allt úr skrá og geymir í Dictionary breytum

--- src/main/ruppskriftir.py
+++ src/main/ruppskriftir.py
@@ -20,10 +20,14 @@ class Ruppskriftir:
     def lesaSkra(self):
         path = self.path
         uppskrift = {}
+        uppskriftUnit = {}
         verd = {}
         einingar = {}
         
         try:
+            #----------------------
+            # opna og lesa úr skrá
+            #----------------------            
             file = open(path, 'r')
             line = file.readline()
             while len(line)> 0:
@@ -35,35 +39,54 @@ class Ruppskriftir:
                     dalkarForm = dalkar
                 if len(dalkar) == 3:
                     dalkarForm = dalkarUpphaf + dalkar
-                #print dalkarForm
                 if (len(dalkar) == 3 or len(dalkar) == 7):
+                    #----------------------
                     # setja inn inn í dictonary
+                    #----------------------
+                    
+                    #----------------------
                     # ef stak 3 og 6 eru eins setjum við inn í einingar
+                    #----------------------
                     if dalkarForm[2] == dalkarForm[6]:
-                        print dalkarForm[2]
+                        #print dalkarForm[2]
                         fractEin = Fraction(int(dalkarForm[4]),int(dalkarForm[0]))
                         eining = dalkarForm[5]
                         unit = dalkarForm[1]
                         tup = [(unit,fractEin,eining)]
                         if einingar.has_key(dalkarForm[2]):
-                            #tupListi = []
                             tupEin = einingar[dalkarForm[2]] + tup
-                            #tupListi.append(tupEin)
-                            #tupEin.append(tup)
                             einingar[dalkarForm[2]]=tupEin
                         else:
                             einingar[dalkarForm[2]]=tup
-      
-
-                #if dalkarForm[2] == dalkarForm[6]:
-                    #print dalkarForm[2]
-                
-                
+                    #----------------------
+                    # setja inn verð á einingum í verd
+                    #----------------------                            
+                    if dalkarForm[6] == "peningar":
+                        verdPerUnit = float(dalkarForm[4])/float(dalkarForm[0])
+                        verd[dalkarForm[2]]=(dalkarForm[1],verdPerUnit)
+                    #----------------------
+                    # setja inn í uppskriftir og uppskriftirUnit
+                    # uppskrfitirUnit halda utan um grunneiningu sem fæst með því að 
+                    # vinna úr uppskrift
+                    #----------------------
+                    if dalkarForm[2] != dalkarForm[6] and dalkarForm[6] != "peningar":
+                        magn = Fraction(int(dalkarForm[4]),int(dalkarForm[0]))
+                        eining = dalkarForm[5]
+                        innih = dalkarForm[6]
+                        tup = [(magn,eining,innih)]
+                        if uppskrift.has_key(dalkarForm[2]):
+                            tupEin = uppskrift[dalkarForm[2]] + tup
+                            uppskrift[dalkarForm[2]]=tupEin
+                        else:
+                            uppskriftUnit[dalkarForm[2]]=(1,dalkarForm[1])
+                            uppskrift[dalkarForm[2]]=tup                            
                 line = file.readline()  
                 
-            #ars = einingar["*"]
             print einingar
             print einingar["basil"][0][0]
+            print verd
+            print uppskrift
+            print uppskriftUnit
                 
         except IOError as (errno):
             print "I/O error({0}): ".format(errno)
